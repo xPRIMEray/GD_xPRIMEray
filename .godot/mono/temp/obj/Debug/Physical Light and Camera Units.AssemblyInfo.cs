5fef6b3a510376cc27cfe46009b548b6a83306f9
Billy <111899168+AetherTopologist@users.noreply.github.com>
2026-01-18 14:03:40 -0600

    VerifiedGRINrefCircles

--- .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2ba061d59ef0d8b0628a1acb026f434ce26268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2a5d0365a585ca6835ca19ea124bacc467edea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
