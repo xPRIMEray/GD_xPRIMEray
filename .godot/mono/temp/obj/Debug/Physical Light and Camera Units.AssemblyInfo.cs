b8d7f3a8a6ddf6fee8aa46ff093010ab517d9e83
Billy <111899168+AetherTopologist@users.noreply.github.com>
2026-01-17 18:01:19 -0600

    removedbuilderror

--- .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9756e4bc2b80c700666260eaeb7f55ac61022c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4435cddf209e30b34369259e296a26ed07248b37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
