73f2d0796d4dc7d18ec71ac8e44597cbb4881ef2
Billy <111899168+AetherTopologist@users.noreply.github.com>
2026-03-10 22:02:27 -0500

    Audit fixture scenes for transport

--- .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad19433e59eae7feb4747350faa2d50ac521004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22576424cae44065c1b94bbd0ccedc531fbb89d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
