ac4394ac7e77158b01af84d24f57e3846b64c0b5
Billy Broch <aethertopologist@gmail.com>
2026-04-14 22:00:31 -0500

    flatten outputs a bit

--- .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26d54a4ff95f3618183de030cc8931da8d00eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050b7802b19469e3c5aa364fbae0febc4552783b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
