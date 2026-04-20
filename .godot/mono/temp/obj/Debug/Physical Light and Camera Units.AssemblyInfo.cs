79d9a508afb10a0c314e1090a8bee892531f80cd
Billy <111899168+AetherTopologist@users.noreply.github.com>
2026-04-20 18:20:28 -0500

    Revert unintended Claude worktree and temp build changes

--- .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe7d5ac23b77a7bd1111641ba404c7eac244ed39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2893406f3e627a7195d0b281197c499f3d30aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
