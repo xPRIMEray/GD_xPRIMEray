73840521f2ef37bcd0537729daff6ba7f6b30fc0
Billy Broch <aethertopologist@gmail.com>
2026-05-16 13:14:23 -0500

    logo files

--- .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Physical Light and Camera Units.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43d7982154a884fc7ab2de3ae630a895eb08b706")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe00d720114673adaf8bef2506d209ea14602a1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Physical Light and Camera Units")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
